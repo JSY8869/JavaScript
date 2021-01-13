ec45510947385398024cc20d6450b6d5bb5f2e97
JSY8869 <65009713+JSY8869@users.noreply.github.com>
2021-01-13 22:31:33 +0900

    연습중....
    연습중....

--- PR/Color.js
+++ PR/Color.js
@@ -18,15 +18,21 @@ var Links = {
 }
 function nightDayHandler(self) {
     var target = document.querySelector('body');
-    if(self.value === 'day') {
+    if(self.value === 'night') {
         Body.setBackgroundColor('black');
         Body.setColor('white');
-        self.value = 'night';
-        Links.setColor('black');
+        self.value = 'day';
+        Links.setColor('white');
+        document.querySelector('div#menu').style.backgroundColor = 'rgb(69, 80, 80)';
+        document.querySelector('div#whole_contents').style.backgroundColor = 'rgb(54, 52, 41)';
+        document.querySelector('div#head_name').style.backgroundColor = 'rgb(65, 60, 68)';
     } else {
         Body.setBackgroundColor('white');
         Body.setColor('black');
-        self.value = 'day';
+        self.value = 'night';
         Links.setColor('blue');
-        }
+        document.querySelector('div#menu').style.backgroundColor = 'rgb(224, 226, 226)';
+        document.querySelector('div#whole_contents').style.backgroundColor = 'rgb(253, 250, 238)';
+        document.querySelector('div#head_name').style.backgroundColor = 'rgb(237, 222, 245)';
     }
+}
